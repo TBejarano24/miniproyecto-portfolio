98b3939e382bb6bde4127699866d00b51d9f39b3
TBejarano24 <bejaranotomas04@gmail.com>
2025-04-21 09:38:07 -0300

    Versión 'finalizada' de la página con diseño en oscuro y con responsive. Hay ciertas fallas en situaciones específicas, pero es lo mejor que pude hacer hasta ahora

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 let headerMenuBlurSet = document.querySelector("#menuButton");
 let headerMenuBlurErase = document.querySelector("#closeMenu");
 let menuHyperLink = document.querySelector(".menuHyperLink")
+let darkModeToggle = document.querySelector("#darkModeToggleButton")
 
 headerMenuBlurSet.addEventListener("click", addHeaderBlur);
 headerMenuBlurErase.addEventListener("click", eraseHeaderBlur);
 menuHyperLink.addEventListener("click", closeSideMenuOnHyperLink)
+darkModeToggle.addEventListener("click", switchDarkMode)
 
 function addHeaderBlur() {
-    document.getElementById("headerElement").classList.replace("bg-[#091021]", "bg-[#0f1627]");
-    document.getElementById("logoCover").classList.add("bg-gray-900/50");
+    if (document.documentElement.classList.contains("dark")) {
+        document.getElementById("headerElement").classList.replace("dark:bg-[#091021]", "dark:bg-[#0f1627]");
+        document.getElementById("logoCover").classList.add("bg-[#0f162780]");
+    }
+    else {
+        document.getElementById("headerElement").classList.replace("bg-white", "bg-[#888c94]");
+        document.getElementById("logoCover").classList.add("bg-[#888c94]");
+    }
 };
 
 function eraseHeaderBlur() {
-    document.getElementById("headerElement").classList.replace("bg-[#0f1627]", "bg-[#091021]");
-    document.getElementById("logoCover").classList.remove("bg-gray-900/50");
+    if (document.documentElement.classList.contains("dark")) {
+        document.getElementById("headerElement").classList.replace("dark:bg-[#0f1627]", "dark:bg-[#091021]");
+        document.getElementById("logoCover").classList.remove("bg-[#0f162780]");
+    }
+    else {
+        document.getElementById("headerElement").classList.replace("bg-[#888c94]", "bg-white");
+        document.getElementById("logoCover").classList.remove("bg-[#888c94]");
+    }
 };
 
 function closeSideMenuOnHyperLink() {
@@ -21,8 +35,18 @@ function closeSideMenuOnHyperLink() {
     document.getElementById("drawer-right-example").removeAttribute("aria-modal");
     document.getElementById("drawer-right-example").removeAttribute("role");
     document.getElementById("drawer-right-example").setAttribute("aria-hidden", "true");
-    document.getElementById("headerElement").classList.replace("bg-[#0f1627]", "bg-[#091021]");
-    document.getElementById("logoCover").classList.remove("bg-gray-900/50");
-    document.body.classList.remove("overflow-hidden")
-    document.querySelector("[drawer-backdrop]").remove()
+    if (document.documentElement.classList.contains("dark")) {
+        document.getElementById("headerElement").classList.replace("bg-[#0f1627]", "bg-[#091021]");
+        document.getElementById("logoCover").classList.remove("bg-[#0f162780]");
+    }
+    else {
+        document.getElementById("headerElement").classList.replace("bg-[#888c94]", "bg-white");
+        document.getElementById("logoCover").classList.remove("bg-[#888c94]");
+    }
+    document.body.classList.remove("overflow-hidden");
+    document.querySelector("[drawer-backdrop]").remove();
+};
+
+function switchDarkMode() {
+    document.documentElement.classList.toggle("dark");
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        dark: "#0D1117",
-        light: "#FFFFFF",
-        highlight: "#E3FF6A",
-        neonGreen: "#A4FF7A",
-        deepBlue: "#1E293B",
-        softGray: "#8892B0",
-      },
-      backgroundImage: {
-        "gradient-main": "linear-gradient(135deg, #1E293B 0%, #0D1117 100%)",
-      },
+    content: ["./*.html"],
+    darkMode: 'class',
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
