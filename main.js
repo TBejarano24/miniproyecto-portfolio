f77b291c5a6ce98adaa44fa24ed59f5134a66a4d
TBejarano24 <bejaranotomas04@gmail.com>
2025-05-01 17:28:54 -0300

    Se resolvieron algunos errores. El desbordamiento horizontal de la página se resolvió al reemplazar el ancho de los elementos que lo tenían configurado en 'screen' a 'full'. El error en el que el fondo no se oscurecía al desplegar el menú después de haber hecho click en alguno de sus enlaces fue resuelto al añadir a cada enlace el atributo 'data-drawer-hide' con el valor 'drawer-right-example'. Los errores relacionados con el cambio de modo oscuro a modo luminoso mientras el menú se hallaba desplegado fueron resueltos al modificar la función pertinente para inhabilitar dicha funcionalidad mientras el menú se hallase desplegado. A continuación se resolverán errores del diseño responsive

--- main.js
+++ main.js
@@ -1,52 +1,70 @@
 let headerMenuBlurSet = document.querySelector("#menuButton");
 let headerMenuBlurErase = document.querySelector("#closeMenu");
-let menuHyperLink = document.querySelector(".menuHyperLink")
-let darkModeToggle = document.querySelector("#darkModeToggleButton")
+let menuHyperLink = document.querySelector(".menuHyperLink");
+let darkModeToggle = document.querySelector("#darkModeToggleButton");
+let menuElement = document.querySelector("#drawer-right-example");
 
 headerMenuBlurSet.addEventListener("click", addHeaderBlur);
 headerMenuBlurErase.addEventListener("click", eraseHeaderBlur);
-menuHyperLink.addEventListener("click", closeSideMenuOnHyperLink)
-darkModeToggle.addEventListener("click", switchDarkMode)
+menuHyperLink.addEventListener("click", closeSideMenuOnHyperLink);
+darkModeToggle.addEventListener("click", switchDarkMode);
 
 function addHeaderBlur() {
-    if (document.documentElement.classList.contains("dark")) {
-        document.getElementById("headerElement").classList.replace("dark:bg-[#091021]", "dark:bg-[#0f1627]");
-        document.getElementById("logoCover").classList.add("bg-[#0f162780]");
-    }
-    else {
-        document.getElementById("headerElement").classList.replace("bg-white", "bg-[#888c94]");
-        document.getElementById("logoCover").classList.add("bg-[#888c94]");
-    }
-};
+  if (document.documentElement.classList.contains("dark")) {
+    document
+      .getElementById("headerElement")
+      .classList.replace("dark:bg-[#091021]", "dark:bg-[#0f1627]");
+    document.getElementById("logoCover").classList.add("bg-[#0f162780]");
+  } else {
+    document
+      .getElementById("headerElement")
+      .classList.replace("bg-white", "bg-[#888c94]");
+    document.getElementById("logoCover").classList.add("bg-[#888c94]");
+  }
+}
 
 function eraseHeaderBlur() {
-    if (document.documentElement.classList.contains("dark")) {
-        document.getElementById("headerElement").classList.replace("dark:bg-[#0f1627]", "dark:bg-[#091021]");
-        document.getElementById("logoCover").classList.remove("bg-[#0f162780]");
-    }
-    else {
-        document.getElementById("headerElement").classList.replace("bg-[#888c94]", "bg-white");
-        document.getElementById("logoCover").classList.remove("bg-[#888c94]");
-    }
-};
+  if (document.documentElement.classList.contains("dark")) {
+    document
+      .getElementById("headerElement")
+      .classList.replace("dark:bg-[#0f1627]", "dark:bg-[#091021]");
+    document.getElementById("logoCover").classList.remove("bg-[#0f162780]");
+  } else {
+    document
+      .getElementById("headerElement")
+      .classList.replace("bg-[#888c94]", "bg-white");
+    document.getElementById("logoCover").classList.remove("bg-[#888c94]");
+  }
+}
 
 function closeSideMenuOnHyperLink() {
-    document.getElementById("drawer-right-example").classList.replace("transform-none", "translate-x-full");
-    document.getElementById("drawer-right-example").removeAttribute("aria-modal");
-    document.getElementById("drawer-right-example").removeAttribute("role");
-    document.getElementById("drawer-right-example").setAttribute("aria-hidden", "true");
-    if (document.documentElement.classList.contains("dark")) {
-        document.getElementById("headerElement").classList.replace("bg-[#0f1627]", "bg-[#091021]");
-        document.getElementById("logoCover").classList.remove("bg-[#0f162780]");
-    }
-    else {
-        document.getElementById("headerElement").classList.replace("bg-[#888c94]", "bg-white");
-        document.getElementById("logoCover").classList.remove("bg-[#888c94]");
-    }
-    document.body.classList.remove("overflow-hidden");
-    document.querySelector("[drawer-backdrop]").remove();
-};
+  document
+    .getElementById("drawer-right-example")
+    .classList.replace("transform-none", "translate-x-full");
+  document.getElementById("drawer-right-example").removeAttribute("aria-modal");
+  document.getElementById("drawer-right-example").removeAttribute("role");
+  document
+    .getElementById("drawer-right-example")
+    .setAttribute("aria-hidden", "true");
+  if (document.documentElement.classList.contains("dark")) {
+    document
+      .getElementById("headerElement")
+      .classList.replace("bg-[#0f1627]", "bg-[#091021]");
+    document.getElementById("logoCover").classList.remove("bg-[#0f162780]");
+  } else {
+    document
+      .getElementById("headerElement")
+      .classList.replace("bg-[#888c94]", "bg-white");
+    document.getElementById("logoCover").classList.remove("bg-[#888c94]");
+  }
+  document.body.classList.remove("overflow-hidden");
+  // document.querySelector("[drawer-backdrop]").remove();
+}
 
 function switchDarkMode() {
+  if (menuElement.getAttribute("aria-hidden")) {
     document.documentElement.classList.toggle("dark");
-};
+  } else {
+    return;
+  }
+}
